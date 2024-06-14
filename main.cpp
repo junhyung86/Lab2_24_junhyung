2c91253235f75034b50b6ee3846949bd57f740e7
JunHyung <tonycho0806@snu.ac.kr>
2024-06-14 12:48:53 +0900

    modify junhyung

--- main.cpp
+++ main.cpp
@@ -6,17 +6,23 @@ using namespace std;
 int N,M;
 vector<int> vec;
 
-void func(int cnt) {
-
-
-
-
-
-
-
+void func(int start, int count) {
+   if(count==M) {
+      for(int i = 0; i<M; ++i){
+	 cout<< vec[i] << endl;
+      }
+      cout << '\n';
+      return;
+   }
+   for (int i = start; i <= N; ++i){
+      vec.push_back(i);
+      func(i+1, count+1);
+      vec.pop_back();
+   }
 }
+	
 
 int main() {
    cin >> N >> M;
-   func(0);
+   func(1,0);
 }
